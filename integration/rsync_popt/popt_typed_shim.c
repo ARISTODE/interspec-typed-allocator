39efbd3e963ba386c8e53c64ccdbfce0cc7ae0b4
Yongzhe_Jack <JK.ARISTODE@gmail.com>
2026-08-26 13:10:14 -0700

    Track popt option arguments before returning to T

--- integration/rsync_popt/popt_typed_shim.c
+++ integration/rsync_popt/popt_typed_shim.c
@@ -1,4 +1,9 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "interspec_popt_u_policy.h"
+#include "popt.h"
 
 typedef uint32_t (*interspec_alloc_fn)(uint32_t, uint32_t);
 
@@ -12,4 +17,21 @@ void interspec_popt_init(interspec_alloc_fn alloc)
 uint32_t typed_alloc(uint32_t size, uint32_t type_id)
 {
   return interspec_alloc ? interspec_alloc(size, type_id) : 0;
+}
+
+char* interspec_popt_get_opt_arg(void* opaque)
+{
+  char* src = poptGetOptArg((poptContext)opaque);
+  if (!src) return NULL;
+
+  const size_t size = strlen(src) + 1;
+  const uint32_t dst = typed_alloc((uint32_t)size, INTERSPEC_TYPE_ID_CHAR);
+  if (!dst) {
+    free(src);
+    return NULL;
+  }
+
+  memcpy((void*)(uintptr_t)dst, src, size);
+  free(src);
+  return (char*)(uintptr_t)dst;
 }
