89161908c9f95f751c3762ffc5de6ceaf61f07db
Yongzhe_Jack <JK.ARISTODE@gmail.com>
2026-08-26 13:10:34 -0700

    Exercise real popt option argument return path

--- integration/rsync_popt/popt_smoke.c
+++ integration/rsync_popt/popt_smoke.c
@@ -4,18 +4,19 @@ static int archive_seen;
 
 static const struct poptOption options[] = {
   {"archive", 'a', POPT_ARG_NONE, &archive_seen, 0, 0, 0},
+  {"target", 't', POPT_ARG_STRING, 0, 't', 0, 0},
   POPT_TABLEEND
 };
 
 void* interspec_popt_parse_smoke(void)
 {
-  const char* argv[] = {"rsync", "--archive", 0};
+  const char* argv[] = {"rsync", "--archive", "--target=destination", 0};
   archive_seen = 0;
 
-  poptContext ctx = poptGetContext("rsync", 2, argv, options, 0);
+  poptContext ctx = poptGetContext("rsync", 3, argv, options, 0);
   if (!ctx) return 0;
 
-  if (poptGetNextOpt(ctx) < -1) return 0;
+  if (poptGetNextOpt(ctx) != 't') return 0;
   return ctx;
 }
 
